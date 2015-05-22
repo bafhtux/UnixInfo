fa9043fa8d43a0a2f01c926ee53e066efe35ed74
Maximilian Sparenberg <maximilian.sparenberg@gmail.com>
2015-05-22 08:43:26 +0200

    commit program

---
+++ src/unixinfo/UnixInfo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unixinfo;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.UnknownHostException;
+ import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+/**
+ *
+ * @author sparenberg
+ */
+public class UnixInfo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        JFrame_Main jframe_main = new JFrame_Main();
+        jframe_main.setVisible(true);
+        
+        
+    
+    }
+    
+    
+}
