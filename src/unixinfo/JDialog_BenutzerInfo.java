465ffcfb0e780252cfdd00a1be7f93b3cfed2f4b
Max.Sparenberg@icloud.com <>
2015-05-22 09:01:00 +0200

    change header

--- src/unixinfo/JDialog_BenutzerInfo.java
+++ src/unixinfo/JDialog_BenutzerInfo.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * (c) 2015 -- Maximilian Sparenberg -- max.sparenberg@icloud.com
+ * All rights reserved
  */
 package unixinfo;
 
--- src/unixinfo/UnixInfo.java
+++ src/unixinfo/UnixInfo.java
@@ -1,20 +1,10 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * (c) 2015 -- Maximilian Sparenberg -- max.sparenberg@icloud.com
+ * All rights reserved
  */
 package unixinfo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.net.UnknownHostException;
- import java.util.Enumeration;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author sparenberg
